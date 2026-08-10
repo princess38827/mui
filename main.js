592803c18775fce933d7fde4a1a0735037b6aa48
Alana Battaile <princess38827@gmail.com>
2026-08-10 05:50:56 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  win.loadFile('renderer/index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+// Placeholder for future IRC connection handling via IPC
+ipcMain.handle('connect-irc', async (event, config) => {
+  // We'll expand this later with real irc-framework code
+  return { success: true, message: 'Connection placeholder' };
+});
